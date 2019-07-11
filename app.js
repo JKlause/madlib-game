8ab80d00b11863464657fbb09ef763e17342d2c5
Joe Klause <joeklause@gmail.com>
2019-07-11 07:52:48 -0700

    functioning java

--- app.js
+++ app.js
@@ -2,25 +2,26 @@
 
 // User word inputs
 const noun1Input = document.getElementById('noun-1');
-const pastTenseAdj1Input = document.getElementById('past-tense-adj-1');
-const pastTenseAdj2Input = document.getElementById('past-tense-adj-2');
+const Adj1Input = document.getElementById('adj-1');
+const Adj2Input = document.getElementById('adj-2');
 const noun2Input = document.getElementById('noun-2');
 const plNoun1Input = document.getElementById('pl-noun-1');
 const verb1Input = document.getElementById('verb-1');
 
 // Writing user words to spans
 const userNoun1 = document.getElementById('user-noun-1');
-const userPastTenseAdj1 = document.getElementById('user-past-tense-adj-1');
-const userPastTenseAdj2 = document.getElementById('user-past-tense-adj-2');
+const userAdj1 = document.getElementById('user-adj-1');
+const userAdj2 = document.getElementById('user-adj-2');
 const userNoun2 = document.getElementById('user-noun-2');
 const userPlNoun1 = document.getElementById('user-pl-noun-1');
 const userVerb1 = document.getElementById('user-verb-1');
 
+// eslint-disable-next-line no-unused-vars
 function createMadLib() {
-    userNoun1.textContent = noun1Input.Value;
-    userPastTenseAdj1.textContent = pastTenseAdj1Input.Value;
-    userPastTenseAdj2.textContent = pastTenseAdj2Input.Value;
-    userNoun2.textContent = noun2Input.Value;
-    userPlNoun1.textContent = plNoun1Input.Value;
-    userVerb1.textContent = verb1Input.Value;
+    userNoun1.textContent = noun1Input.value;
+    userAdj1.textContent = Adj1Input.value;
+    userAdj2.textContent = Adj2Input.value;
+    userNoun2.textContent = noun2Input.value;
+    userPlNoun1.textContent = plNoun1Input.value;
+    userVerb1.textContent = verb1Input.value;
 }
